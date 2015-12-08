3469391b76ad260a7d64ba33f6af51de7fb14fb2
poiintman <bwills91686@hotmail.com>
2015-12-08 12:25:01 -0500

    Create login.php

---
+++ Internet-Programming-Lab-4/login.php
@@ -0,0 +1,6 @@
+<?php // login.php file that contains the database information
+  $hn = 'localhost'; // host name
+  $db = 'inventory'; // database name
+  $un = 'root'; //directory
+  $pw = ''; // password
+?>
